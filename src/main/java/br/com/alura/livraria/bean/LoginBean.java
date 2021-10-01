f1209fd20aa3f3e992f3c0fccfbf3f1f83b453fd
Matheus Medeiros <mmr94@live.com>
2021-10-01 17:21:51 -0300

    Criando autorização pelo PhaseListener, Logout e escopo Flash

--- src/main/java/br/com/alura/livraria/bean/LoginBean.java
+++ src/main/java/br/com/alura/livraria/bean/LoginBean.java
@@ -1,7 +1,9 @@
 package br.com.alura.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.alura.livraria.dao.UsuarioDao;
 import br.com.alura.livraria.modelo.Usuario;
@@ -19,14 +21,25 @@ public class LoginBean {
 	public String logar() {
 		
 		boolean existe = new UsuarioDao().existe(this.usuario);
+		FacesContext context = FacesContext.getCurrentInstance();
 		if(existe) {
-			
-		return "livro?faces-redirect=true";
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
 		}
 		
-		return null;
-		}
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 		
+		return "login?faces-redirect";
+		}
+	
+	public String deslogar() {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
 	}
+		
+}
 
 
